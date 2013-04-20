cf2a9b96c0257abca0ff63ae86cdf2b99c24d586
unknown <=>
2013-04-20 11:24:24 +1000

    HudMap Class coding completed (I hope no errors)

--- Minotour/src/mygame/HudMap.java
+++ Minotour/src/mygame/HudMap.java
@@ -1,102 +1,83 @@
-/*
- * To change @author Cesar Frederick Tan this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame;
 
 import de.lessvoid.nifty.Nifty;
-import de.lessvoid.nifty.elements.render.ImageRenderer;
-//import de.lessvoid.nifty.elements.render.TextRenderer;
-import de.lessvoid.nifty.render.NiftyImage;
 
 /**
  *@author Cesar Frederick Tan
- * date written: April 12, 2013
+ * Date written: April 12, 2013
+ * Last Update:  April 20, 2013
  */
-public class HudMap {
-    private static int curRow, curCol;
+
+public class HudMap 
+{
+    private static final int 
+            MAX_HEIGHT = 100, 
+            MAX_WIDTH = 100, 
+            MIN_HEIGHT = 0, 
+            MIN_WIDTH = 0;
+    private static int 
+            curRow, 
+            curCol;
     private static Nifty hudNifty;
-    private static NiftyImage playerPos;
    
     public HudMap(Nifty passNifty)
     {
         hudNifty = passNifty;
         curRow = 0;
         curCol = 0;
-        playerPos = hudNifty.getRenderEngine().createImage("Interface/playerLoc.png", false);
-        //hudNifty.getCurrentScreen().findElementByName("txtLife").getRenderer(TextRenderer.class).setText(Integer.toString(playerLife));
     }
     
-    private void updateLifeBar()
+    private void PutInColumn(int NewColumn)
     {
-        if (playerLife > CRITICAL_LEVEL)
-            hudNifty.getCurrentScreen().findElementByName("lifeBar").getRenderer(ImageRenderer.class).setImage(okeyLevel);
-        else
-            hudNifty.getCurrentScreen().findElementByName("lifeBar").getRenderer(ImageRenderer.class).setImage(criticalLevel);
-       
-        int intLife = playerLife * MAX_LIFEBAR_WIDTH / MAX_HEALTH;
-        //hudNifty.getCurrentScreen().findElementByName("txtLife").getRenderer(TextRenderer.class).setText(Integer.toString(intLife));
-        hudNifty.getCurrentScreen().findElementByName("lifeBar").setWidth(intLife);
-    }
-
-    public void plasmaCaptured()
-    {
-        String eName;
-        if (takenPlasma < TOTAL_PLASMA)
+        if (NewColumn <= MAX_WIDTH & NewColumn >= MIN_WIDTH )
         {
-            takenPlasma += 1;
-            eName = "pc"+ Integer.toString(takenPlasma);
-            hudNifty.getCurrentScreen().findElementByName(eName).getRenderer(ImageRenderer.class).setImage(plasmaOnImage);
+            curCol = NewColumn;
+            hudNifty.getCurrentScreen().findElementByName("ColPos").setWidth(curCol);
         }
     }
     
-    public boolean canTakeMoreHealthPack()
+    private void PutInRow(int NewRow)
     {
-        return (takenHpack < TOTAL_HPACK);
-    }
-    
-    public boolean playerIsDead()
-    {
-        return (playerLife <= 0);
-    }
-    
-    public boolean plasmaComplete()
-    {
-        return (takenPlasma >= TOTAL_PLASMA);
-    }
-    
-    public void healthPackCaptured()
-    {
-        String eName;
-        if (takenHpack < TOTAL_HPACK)
+        if (NewRow <= MAX_HEIGHT & NewRow >= MIN_HEIGHT)
         {
-            takenHpack += 1;
-            eName = "hp" + Integer.toString(takenHpack);
-            hudNifty.getCurrentScreen().findElementByName(eName).getRenderer(ImageRenderer.class).setImage(hpackOnImage);
+            curRow = NewRow;
+            hudNifty.getCurrentScreen().findElementByName("RowPos").setHeight(curRow);
         }
     }
-
-    public int looseLife(int percent)
+    
+    public void MovePlayerOn(int NewRow, int NewColumn)
     {
-        playerLife -= percent;
-        if (playerLife < 0)
-            playerLife = 0;  // this may trigger end Game.
-        updateLifeBar();
-        return playerLife;
+        PutInRow(NewRow);
+        PutInColumn(NewColumn);
     }
     
-    public void consumeHealthPack()
+    public void GoUpBy(int Steps)
     {
-        String eName;
-        if (takenHpack > 0 && playerLife < MAX_HEALTH)
-        {
-            eName = "hp" + Integer.toString(takenHpack);
-            hudNifty.getCurrentScreen().findElementByName(eName).getRenderer(ImageRenderer.class).setImage(hpackOffImage);
-            takenHpack -= 1;            
-            playerLife += HPACKGAIN;
-            if (playerLife > MAX_HEALTH) 
-                playerLife = MAX_HEALTH;
-            updateLifeBar();
-        }
+        PutInRow(curRow + Steps);
+    }
+ 
+    public void GoDownBy(int Steps)
+    {
+        PutInRow(curRow - Steps);
+    }
+    
+    public void GoLeftBy(int Steps)
+    {
+        PutInColumn(curCol - Steps);
+    }
+    
+    public void GoRightBy(int Steps)
+    {
+        PutInColumn(curCol + Steps);
+    }
+    
+    public int GetRow()
+    {
+        return curRow;
+    }
+    
+    public int GetColumn()
+    {
+        return curCol;
     }
 }
